3e09823bdedb39ce3268690556246d473771f32c
Max Hutchinson <max.hutchinson258@gmail.com>
2024-06-18 17:46:09 +0100

    Multi Repo Commit test

--- AspirePoC.BookService/Program.cs
+++ AspirePoC.BookService/Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 builder.Services
     .AddGraphQLServer()
     .AddTypes();
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 app.MapGraphQL();
 
