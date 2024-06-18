1c7881b53678d5f85d469dbf839006328e42c315
Max Hutchinson <max.hutchinson258@gmail.com>
2024-06-18 13:36:31 +0100

    Add project files.

---
+++ AspirePoC.BookService/Types/Author.cs
@@ -0,0 +1,3 @@
+namespace AspirePoC.BookService.Types;
+
+public record Author(string Name);
---
+++ AspirePoC.BookService/Types/Book.cs
@@ -0,0 +1,3 @@
+namespace AspirePoC.BookService.Types;
+
+public record Book(string Title, Author Author);
